b9c74e435e259329208a4c58bbd5d842ac200d78
Undying-Merch <jonasnikolailj@gmail.com>
2022-05-24 13:14:47 +0200

    Re-Fixed 'ALLOWED_HOST'
    Added 127.0.0.1 ip as ALLOWED_HOST to allow to access local_host via computer.

--- apiVesuvius/apiVesuvius/settings.py
+++ apiVesuvius/apiVesuvius/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oalv=8k@!4#l6#(uk1hn#=o=%yli*vw#$y59k1cys3ymh)b*@8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.2']
+ALLOWED_HOSTS = ['10.0.2.2', '127.0.0.1']
 
 
 # Application definition
